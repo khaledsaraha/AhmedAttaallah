dfb7749f9f8c240df246d6c83b9ae19d14673688
ahmed ataallah <ahmed45316@hotmail.com>
2019-11-01 14:11:49 +0200

    UnitOfWork

---
+++ DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //ICourseRepository Courses { get; }
+        int SaveChanges();
+    }
+}
---
+++ DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+           // Courses = new CourseRepository(_context);
+        }
+
+        //public ICourseRepository Courses { get; private set; }
+       
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
