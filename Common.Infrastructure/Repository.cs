55118224e025e37103895075976cd5a09e525123
ahmed ataallah <ahmed45316@hotmail.com>
2019-11-01 14:01:15 +0200

    Repository

---
+++ Common.Infrastructure/Repository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Common.Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+        private readonly DbSet<T> DbSet; 
+        public Repository(DbContext context)
+        {
+            Context = context;
+            DbSet = Context.Set<T>();
+        }
+
+        public T Get(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return DbSet.ToList();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.Where(predicate);
+        }
+
+        public T SingleOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.SingleOrDefault(predicate);
+        }
+
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            DbSet.AddRange(entities);
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            DbSet.RemoveRange(entities);
+        }
+    }
+}
